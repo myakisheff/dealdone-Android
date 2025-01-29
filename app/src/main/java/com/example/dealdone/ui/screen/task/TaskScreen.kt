63719275e0b1aae6a51a45ae2112ac2f61c913a3
myakisheff <n.s.myakishev@yandex.ru>
2025-01-29 21:37:35 +0300

    completed MVP version of screen with tasks

--- app/src/main/java/com/example/dealdone/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/example/dealdone/ui/screen/task/TaskScreen.kt
@@ -1,21 +1,28 @@
-package com.example.dealdone.ui.screen
+package com.example.dealdone.ui.screen.task
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -23,32 +30,86 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dealdone.R
-import com.example.dealdone.data.model.ExampleTasks
 import com.example.dealdone.data.model.TaskInfo
+import com.example.dealdone.data.model.TasksMock
+import com.example.dealdone.ui.component.TaskCard
 import com.example.dealdone.ui.theme.DealDoneTheme
+import java.util.UUID
 
 @Composable
 fun TaskScreen(
-    currentTask: TaskInfo,
+    taskUiState: TaskUiState,
     subtasks: List<TaskInfo>,
+    onTaskClick: (UUID) -> Unit,
+    expandTask: () -> Unit,
+    onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-    ) {
-        CurrentTaskInformation(currentTask = currentTask)
+    Scaffold(
+        topBar = { TaskScreenTopBar(
+            taskName = taskUiState.selectedTask?.name ?: stringResource(R.string.tasks),
+            canNavigateBack = taskUiState.selectedTask != null,
+            onBackClick = onBackClick
+        ) }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = modifier
+                .padding(innerPadding)
+                .padding(dimensionResource(R.dimen.small_padding))
+        ) {
+            if(taskUiState.selectedTask != null){
+                item {
+                    CurrentTaskInformation(
+                        currentTask = taskUiState.selectedTask,
+                        isExpanded = taskUiState.isExpanded,
+                        expandClick = expandTask
+                    )
+                }
+            }
 
+            items(subtasks) { task ->
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.medium_padding)))
+                TaskCard(
+                    task = task,
+                    onClick = { onTaskClick(task.id) }
+                )
+            }
+        }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CurrentTaskInformation(currentTask: TaskInfo) {
-    var isExpanded by remember { mutableStateOf(false) }
+fun TaskScreenTopBar(
+    taskName: String,
+    canNavigateBack: Boolean,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = taskName)},
+        navigationIcon = {
+            if(canNavigateBack) {
+                IconButton(onClick = onBackClick) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
 
+@Composable
+fun CurrentTaskInformation(
+    currentTask: TaskInfo,
+    isExpanded: Boolean,
+    expandClick: () -> Unit
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { isExpanded = !isExpanded },
+            .clickable { expandClick() },
         elevation = CardDefaults.cardElevation(
             defaultElevation = dimensionResource(R.dimen.medium_elevation)
         ),
@@ -110,7 +171,11 @@ fun CurrentTaskInformation(currentTask: TaskInfo) {
 @Composable
 fun CurrentTaskPreview() {
     DealDoneTheme {
-        CurrentTaskInformation(currentTask = ExampleTasks.getRandomTask())
+        CurrentTaskInformation(
+            currentTask = TasksMock.getRandomTask()[0],
+            expandClick = {},
+            isExpanded = false
+        )
     }
 }
 
@@ -119,15 +184,11 @@ fun CurrentTaskPreview() {
 fun TaskScreenPreview() {
     DealDoneTheme {
         TaskScreen(
-            currentTask = ExampleTasks.getRandomTask(),
-            subtasks = listOf(
-                ExampleTasks.getRandomTask(),
-                ExampleTasks.getRandomTask(),
-                ExampleTasks.getRandomTask(),
-                ExampleTasks.getRandomTask(),
-                ExampleTasks.getRandomTask(),
-                ExampleTasks.getRandomTask()
-            )
+            taskUiState = TaskUiState(),
+            expandTask = {},
+            onTaskClick = {},
+            onBackClick = {},
+            subtasks = TasksMock.getRandomTask(number = 7)
         )
     }
 }
