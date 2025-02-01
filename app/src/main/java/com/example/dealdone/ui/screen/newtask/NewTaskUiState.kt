ee8f2c5940a263f00703565d632fa993b71108fe
myakisheff <n.s.myakishev@yandex.ru>
2025-02-01 22:52:16 +0300

    extended ui state for a new task

--- app/src/main/java/com/example/dealdone/ui/screen/newtask/NewTaskUiState.kt
+++ app/src/main/java/com/example/dealdone/ui/screen/newtask/NewTaskUiState.kt
@@ -1,6 +1,28 @@
 package com.example.dealdone.ui.screen.newtask
 
+import com.example.dealdone.data.model.TaskInfo
+import com.example.dealdone.data.model.TaskPriority
+import com.example.dealdone.data.model.TaskStatus
+import java.util.Calendar
+import java.util.UUID
+
 data class NewTaskUiState(
     val fastCreationTaskText: String = "",
     val isFastModeOfCreationTask: Boolean = false,
+)
+
+data class NewDefaultTaskUiState(
+    val creationTask: TaskInfo = TaskInfo(
+        id = UUID.randomUUID(),
+        name = "",
+        description = "",
+        targetDate = null,
+        creationDate = Calendar.getInstance(),
+        taskPriority = TaskPriority.LOW,
+        taskStatus = TaskStatus.IN_PROGRESS,
+        parentTaskID = null
+    ),
+    val isInfinityTime: Boolean = false,
+    val isTimePickerShowing: Boolean = false,
+    val isDatePickerShowing: Boolean = false,
 )
