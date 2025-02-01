69af40de9645653e1b933cbd5b8687ebca556d3d
myakisheff <n.s.myakishev@yandex.ru>
2025-02-01 22:49:50 +0300

    added titles

--- app/src/main/java/com/example/dealdone/data/model/TaskPriority.kt
+++ app/src/main/java/com/example/dealdone/data/model/TaskPriority.kt
@@ -1,5 +1,10 @@
 package com.example.dealdone.data.model
 
-enum class TaskPriority {
-    LOW, MEDIUM, HIGH
+import androidx.annotation.StringRes
+import com.example.dealdone.R
+
+enum class TaskPriority(@StringRes val title: Int) {
+    LOW(title = R.string.low_priority),
+    MEDIUM(title = R.string.medium_priority),
+    HIGH(title = R.string.high_priority)
 }
