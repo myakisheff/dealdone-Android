e9318e2cc52eb71176a5f962bec1323440a3aacb
myakisheff <n.s.myakishev@yandex.ru>
2025-02-06 21:36:31 +0300

    added extension to convert tasks ui

--- app/src/main/java/com/example/dealdone/ui/screen/task/TasksUiState.kt
+++ app/src/main/java/com/example/dealdone/ui/screen/task/TasksUiState.kt
@@ -1,6 +1,10 @@
 package com.example.dealdone.ui.screen.task
 
 import com.example.dealdone.data.model.TaskInfo
+import com.example.dealdone.data.model.TaskPriority
+import com.example.dealdone.data.model.TaskStatus
+import com.example.dealdone.ui.screen.newtask.NewDefaultTaskUiState
+import java.util.Calendar
 
 sealed interface TasksUiState {
     data class Success(val tasks: List<TaskInfo>) : TasksUiState
@@ -12,4 +16,21 @@ sealed interface TasksUiState {
 data class TaskUiState (
     val selectedTask: TaskInfo? = null,
     val isExpanded: Boolean = false,
+    val isEditMode: Boolean = false,
+)
+
+fun TaskUiState.toNewDefaultTaskUiState() : NewDefaultTaskUiState = NewDefaultTaskUiState(
+    creationTask = selectedTask ?: TaskInfo(
+        id = 0,
+        name = "",
+        description = "",
+        targetDate = null,
+        creationDate = Calendar.getInstance(),
+        taskPriority = TaskPriority.LOW,
+        taskStatus = TaskStatus.IN_PROGRESS,
+        parentTaskID = null
+    ),
+    isInfinityTime = selectedTask?.targetDate == null,
+    isTimePickerShowing = false,
+    isDatePickerShowing = false,
 )
